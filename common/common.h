cd65d97d3af313567af3b9d9a96d62d9cd93305e
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2016-12-07 09:48:18 -0700

    common.h is a commonly used header file

---
+++ common/common.h
@@ -0,0 +1,13 @@
+#ifndef __COMMON_H
+#define __COMMON_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Boolean;
+
+#define TRUE 1
+#define FALSE 0
+
+#endif /*__COMMON_H*/
